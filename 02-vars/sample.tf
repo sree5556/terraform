a83d2fd56e696a7472ca119e5103c1a5a253c4ea
KalluruS <>
2020-10-20 07:44:45 +0530

    Update terraform/

---
+++ 02-vars/sample.tf
@@ -0,0 +1,27 @@
+// This is a string
+variable "color" {
+  default = "Red"
+}
+
+// This is a number, can be float (1.111)
+variable "colorcode" {
+  default = 31
+}
+
+// This is a boolean, can be true or false
+variable "bool" {
+  default = true
+}
+
+output "COLOR" {
+  value = var.color
+}
+
+output "COLORCODE" {
+  value = var.colorcode
+}
+
+output "BOOLAEAN" {
+  value = var.bool
+}
+
