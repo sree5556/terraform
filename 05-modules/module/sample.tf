eb7be5cf1f396e2ee30184a7eb3d5d8021d006b9
KalluruS <>
2020-10-20 08:58:23 +0530

    Update terraform/

--- 05-modules/module/sample.tf
+++ 05-modules/module/sample.tf
@@ -1,6 +1,6 @@
 resource "local_file" "foo" {
   content     = "Line Number = ${var.line_number}\n"
-  filename = "/tmp/foo.bar"
+  filename = "/tmp/foo-${var.line_number}.bar"
 }
 
 variable "line_number" {}
