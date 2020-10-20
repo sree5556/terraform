527da97571631568a31bc6299c148324d9ecba13
KalluruS <>
2020-10-20 08:19:13 +0530

    Update terraform/

--- 04-resources/sample.tf
+++ 04-resources/sample.tf
@@ -4,3 +4,9 @@ resource "aws_instance" "sample" {
   ami = "ami-0ed9d2e6b42fd9de7"
   instance_type = "t2.micro"
 }
+
+// Here ami is a argument, instance_type is an argument
+
+output "PUBLIC_IP" {
+  value = aws_instance.sample.public_ip
+}
